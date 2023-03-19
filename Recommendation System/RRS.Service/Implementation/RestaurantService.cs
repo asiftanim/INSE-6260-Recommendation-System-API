8d97a1c30fa884e190b3e4b8d8b1cda51523a556
Asif Tanim <tanimasif23@gmail.com>
2023-03-19 17:57:35 -0400

    Algorithm

--- Recommendation System/RRS.Service/Implementation/RestaurantService.cs
+++ Recommendation System/RRS.Service/Implementation/RestaurantService.cs
@@ -49,5 +49,56 @@ namespace RRS.Service
         {
             return _restaurantsRespository.GetRandomRestaurants();
         }
+
+        private double CalculatePearsonCorrelationCoefficient(double[] CurrentUser, double[] User)
+        {
+            double CurrentUserAverageRating = CurrentUser.Sum() / CurrentUser.Length;
+            double UserAverageRating = User.Sum() / User.Length;
+            //Console.WriteLine($"{CurrentUserAverageRating} - {UserAverageRating}");
+
+            double DividendSum = 0;
+            double Divisor = 0;
+            double DivisorLeft = 0;
+            double DivisorRight = 0;
+
+            for (int i = 0; i < CurrentUser.Length; i++)
+            {
+                DividendSum += (CurrentUser[i] - CurrentUserAverageRating) * (User[i] - UserAverageRating); ;
+
+            }
+            //Console.WriteLine($"DividendSum = {DividendSum}");  //2
+
+
+            for (int i = 0; i < CurrentUser.Length; i++)
+            {
+                DivisorLeft += Math.Pow((CurrentUser[i] - CurrentUserAverageRating), 2);
+            }
+            //Console.WriteLine($"DivisorLeft = {DivisorLeft}"); //2
+
+
+            for (int i = 0; i < CurrentUser.Length; i++)
+            {
+                DivisorRight += Math.Pow((User[i] - UserAverageRating), 2);
+            }
+            //Console.WriteLine($"DivisorRight = {DivisorRight}");//3.2
+
+
+            Divisor = Math.Sqrt(DivisorLeft) * Math.Sqrt(DivisorRight); // 1.42 * 1.79
+            //Console.WriteLine($"Divisor = {Divisor}");
+            double PCCResult = Math.Round(DividendSum / Divisor, 2);
+
+            return PCCResult;
+        }
+
+        private double CalculatePrediction(double CUAR, double User1PCC, double User2PCC, double U1AR, double U2AR, double[] CompareValues)
+        {
+            Console.WriteLine($"CUAR : {CUAR}");
+            Console.WriteLine($"User1PCC : {User1PCC}");
+            Console.WriteLine($"User2PCC : {User2PCC}");
+            Console.WriteLine($"U1AR : {U1AR}");
+            Console.WriteLine($"U2AR : {U2AR}");
+            Console.WriteLine($"CompareValues : {CompareValues[0]} - {CompareValues[1]}");
+            return Math.Round(CUAR + (1 / (User1PCC + User2PCC)) * (User1PCC * (CompareValues[0] - U1AR) + User2PCC * (CompareValues[1] - U2AR)), 2);
+        }
     }
 }
